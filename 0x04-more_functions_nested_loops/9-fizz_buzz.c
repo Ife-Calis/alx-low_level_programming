c6d8e7c7c067b1f5983da3d9552b8b558604e6c8
Ife-Calis <calistusifechukwu39@gmail.com>
2023-07-22 00:58:19 -0700

    9. Fizz-Buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+
+/**
+ *main - fund prints the numbers from 1-100, but for multiples of 5
+ *prints Buzz each numbers of 3 and 5.
+ *Return: Always 0.
+ */
+
+int main(void)
+{
+
+	int num;
+
+	for (num = 1; num <= 100; num++)
+	{
+		if ((num % 3) == 0 && (num & 5) == 0)
+			printf("FizzBuzz");
+
+		else if ((num % 3) == 0)
+			printf("Fizz");
+
+		else if ((num % 5) == 0)
+			printf("Buzz");
+
+		else
+			printf("%d", num);
+
+		if (num == 100)
+			continue;
+		printf(" ");
+	}
+
+	printf("\n");
+
+	return (0);
+}
+
