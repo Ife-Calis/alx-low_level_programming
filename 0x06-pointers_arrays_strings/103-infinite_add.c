e5d54fda267988fe7b25cbd95d5869e4f32a7aa2
Ife-Calis <calistusifechukwu39@gmail.com>
2023-07-30 14:45:11 -0700

    11. It is the addition of strangeness to beauty that constitutes the romantic character in art

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,20 +1,67 @@
 #include "main.h"
 
-#include <stdio.h>
 
 /**
 
- * infinite_add - adds two numbers
+ * rev_string - reverse array
 
- * @n1: number one.
+ * @n: integer params
 
- * @n2: number two.
+ * Return: 0
 
- * @r: buffer that the function will use to store the result.
+ */
 
- * @size_r: buffer size:
 
- * Return: the pointer to dest.
+void rev_string(char *n)
+
+{
+
+        int i = 0;
+
+        int j = 0;
+
+        char temp;
+
+
+        while (*(n + i) != '\0')
+
+        {
+
+                i++;
+
+        }
+
+        i--;
+
+
+        for (j = 0; j < i; j++, i--)
+
+        {
+
+                temp = *(n + j);
+
+                *(n + j) = *(n + i);
+
+                *(n + i) = temp;
+
+        }
+
+}
+
+
+/**
+
+ * infinite_add - add 2 numbers together
+
+ * @n1: text representation of 1st number to add
+
+ * @n2: text representation of 2nd number to add
+
+ * @r: pointer to buffer
+
+ * @size_r: buffer size
+
+ * Return: pointer to calling function
 
  */
 
@@ -23,84 +70,79 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 
 {
 
-        int c1 = 0, c2 = 0, op, bg, dr1, dr2, add = 0;
+        int overflow = 0, i = 0, j = 0, digits = 0;
+
+        int val1 = 0, val2 = 0, temp_tot = 0;
 
 
-        while (*(n1 + c1) != '\0')
+        while (*(n1 + i) != '\0')
 
-                c1++;
+                i++;
 
-        while (*(n2 + c2) != '\0')
+        while (*(n2 + j) != '\0')
 
-                c2++;
+                j++;
 
-        if (c1 >= c2)
+        i--;
 
-                bg = c1;
+        j--;
 
-        else
-
-                bg = c2;
-
-        if (size_r <= bg + 1)
+        if (j >= size_r || i >= size_r)
 
                 return (0);
 
-        r[bg + 1] = '\0';
-
-        c1--, c2--, size_r--;
-
-        dr1 = *(n1 + c1) - 48, dr2 = *(n2 + c2) - 48;
-
-        while (bg >= 0)
+        while (j >= 0 || i >= 0 || overflow == 1)
 
         {
 
-                op = dr1 + dr2 + add;
+                if (i < 0)
 
-                if (op >= 10)
-
-                        add = op / 10;
+                        val1 = 0;
 
                 else
 
-                        add = 0;
+                        val1 = *(n1 + i) - '0';
 
-                if (op > 0)
+                if (j < 0)
 
-                *(r + bg) = (op % 10) + 48;
-
-                else
-
-                        *(r + bg) = '0';
-
-                if (c1 > 0)
-
-                        c1--, dr1 = *(n1 + c1) - 48;
+                        val2 = 0;
 
                 else
 
-                        dr1 = 0;
+                        val2 = *(n2 + j) - '0';
 
-                if (c2 > 0)
+                temp_tot = val1 + val2 + overflow;
 
-                        c2--, dr2 = *(n2 + c2) - 48;
+                if (temp_tot >= 10)
+
+                        overflow = 1;
 
                 else
 
-                        dr2 = 0;
+                        overflow = 0;
 
-                bg--, size_r--;
+                if (digits >= (size_r - 1))
+
+                        return (0);
+
+                *(r + digits) = (temp_tot % 10) + '0';
+
+                digits++;
+
+                j--;
+
+                i--;
 
         }
 
-        if (*(r) == '0')
+        if (digits == size_r)
 
-                return (r + 1);
+                return (0);
 
-        else
+        *(r + digits) = '\0';
 
-                return (r);
+        rev_string(r);
+
+        return (r);
 
 }
-
