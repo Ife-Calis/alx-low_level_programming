6f508ed9993006eebc8e47e83e9cbf624684c471
Ife-Calis <calistusifechukwu39@gmail.com>
2023-07-31 08:30:09 -0700

    11. It is the addition of strangeness to beauty that constitutes the romantic character in art

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,18 +1,15 @@
 #include "main.h"
 
 /**
-* rev_string - reverse array
-* @n: integer params
-* Return: 0
+ * rev_string - reverse array
+ * @n: integer params
+ * Return: 0
  */
 
 void rev_string(char *n)
 {
-
 	int i = 0;
-
 	int j = 0;
-
 	char temp;
 
 	while (*(n + i) != '\0')
@@ -20,11 +17,12 @@ void rev_string(char *n)
 		i++;
 	}
 	i--;
+
 	for (j = 0; j < i; j++, i--)
 	{
-	temp = *(n + j);
-	*(n + j) = *(n + i);
-	*(n + i) = temp;
+		temp = *(n + j);
+		*(n + j) = *(n + i);
+		*(n + i) = temp;
 	}
 }
 
@@ -39,9 +37,7 @@ void rev_string(char *n)
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-
 	int overflow = 0, i = 0, j = 0, digits = 0;
-
 	int val1 = 0, val2 = 0, temp_tot = 0;
 
 	while (*(n1 + i) != '\0')
@@ -54,7 +50,7 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 		return (0);
 	while (j >= 0 || i >= 0 || overflow == 1)
 	{
-	if (i < 0)
+		if (i < 0)
 			val1 = 0;
 		else
 			val1 = *(n1 + i) - '0';
@@ -62,20 +58,20 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 			val2 = 0;
 		else
 			val2 = *(n2 + j) - '0';
-			temp_tot = val1 + val2 + overflow;
+		temp_tot = val1 + val2 + overflow;
 		if (temp_tot >= 10)
 			overflow = 1;
 		else
 			overflow = 0;
 		if (digits >= (size_r - 1))
-		return (0);
+			return (0);
 		*(r + digits) = (temp_tot % 10) + '0';
 		digits++;
 		j--;
 		i--;
 	}
 	if (digits == size_r)
-	return (0);
+		return (0);
 	*(r + digits) = '\0';
 	rev_string(r);
 	return (r);
